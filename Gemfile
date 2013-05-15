92ac5873715e4dac4751a4b987eb2452548b8797
Bryan Chang <keunyoung.chang@gmail.com>
2013-05-15 05:01:31 -0700

    Add Integration Test for Main Page by installing Capybara Gem, bundle, and making new spec directory and file

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'sqlite3'
 group :test do
   gem 'simplecov'
   gem 'rspec-rails'
+  gem 'capybara'
 end
 
 
---
+++ spec/features/main_page_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "main page" do
+
+  it "displays the title of the page" do
+    visit '/todo_items'
+    expect(page).to have_selector('h1', text: "Create a new TODO")
+  end
+
+end
